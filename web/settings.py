4d6018bc7664d92807039233f3d8b1299962e1f7
meinliebe <ginus_pandhu@hotmail.com>
2016-12-24 22:58:40 +0700

    settings.py

--- web/settings.py
+++ web/settings.py
@@ -75,14 +75,18 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-		'NAME': 'ongkir',
-		'USER': 'admin',
-		'PASSWORD': 'admin',
-		'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-		'PORT': '5432',
-	}
+	# 'default': {
+	# 	'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+	# 	'NAME': 'ongkir',
+	# 	'USER': 'admin',
+	# 	'PASSWORD': 'admin',
+	# 	'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+	# 	'PORT': '5432',
+	# }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }	
 }
 
 
